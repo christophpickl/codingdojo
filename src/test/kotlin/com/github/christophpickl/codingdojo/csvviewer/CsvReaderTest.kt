0ec20d89bb4a54dec6d065b4db539b347ed1e7c7
christophpickl <christoph.pickl@gmail.com>
2018-11-20 20:54:49 +0100

    reformat test

--- src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvReaderTest.kt
+++ src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvReaderTest.kt
@@ -7,17 +7,18 @@ import org.testng.annotations.Test
 @Test
 class CsvReaderTest {
 
-    private val sampleData = """
-        H1;H2
-        a1;a2
-        b1;b2
-    """.trimIndent()
-
     fun `When read sample data Then return proper structure`() {
-        assertThat(CsvReader.read(sampleData),
+        assertThat(CsvReader.read("""
+            H1;H2
+            a1;a2
+            b1;b2
+        """.trimIndent()),
             equalTo(CsvTable(
-                listOf("H1", "H2"),
-                listOf(listOf("a1", "a2"), listOf("b1", "b2"))
+                headers = listOf("H1", "H2"),
+                rowData = listOf(
+                    listOf("a1", "a2"),
+                    listOf("b1", "b2")
+                )
             )))
     }
 
