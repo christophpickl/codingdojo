4f6bb7d24d5adb9de0bc6c5b4346ce3f9ce8e6cf
christophpickl <christoph.pickl@gmail.com>
2018-11-20 21:04:25 +0100

    add failing test for dynamic row length

--- src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvFormatterTest.kt
+++ src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvFormatterTest.kt
@@ -7,7 +7,7 @@ import org.testng.annotations.Test
 @Test
 class CsvFormatterTest {
 
-    fun `foo bar`() {
+    fun `When format table without whitespaces Then return proper string`() {
         assertThat(CsvFormatter.format(CsvTable(
             headers = listOf("H1", "H2"),
             rowData = listOf(
@@ -19,6 +19,36 @@ class CsvFormatterTest {
             --+--
             a1|a2
             b1|b2
+        """.trimIndent()))
+    }
+
+    fun `When header has over length Then indent rows of first column`() {
+        assertThat(CsvFormatter.format(CsvTable(
+            headers = listOf("H1a", "H2"),
+            rowData = listOf(
+                listOf("a1", "a2"),
+                listOf("b1", "b2")
+            )
+        )), equalTo("""
+            H1a|H2
+            ---+--
+            a1 |a2
+            b1 |b2
+        """.trimIndent()))
+    }
+
+    fun `When row has over length Then indent rows of first column including header`() {
+        assertThat(CsvFormatter.format(CsvTable(
+            headers = listOf("H1", "H2"),
+            rowData = listOf(
+                listOf("a1a", "a2"),
+                listOf("b1", "b2")
+            )
+        )), equalTo("""
+            H1 |H2
+            ---+--
+            a1a|a2
+            b1 |b2
         """.trimIndent()))
     }
 
