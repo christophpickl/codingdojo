a1dfa339ccdf1e6f6e22af57ce4405e430bb62e9
Christoph Pickl <christoph.pickl@erstegroup.com>
2018-11-20 10:42:13 +0100

    prepare wordcount app

---
+++ src/main/kotlin/com/github/christophpickl/codingdojo/wordcount/WordCountApp.kt
@@ -0,0 +1,8 @@
+package com.github.christophpickl.codingdojo.wordcount
+
+object WordCountApp {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("hello word count app!")
+    }
+}
