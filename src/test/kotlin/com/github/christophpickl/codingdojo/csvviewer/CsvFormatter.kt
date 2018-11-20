baf88cdd86d1da8958817973cf359ccb9d26b3fe
christophpickl <christoph.pickl@gmail.com>
2018-11-20 21:27:52 +0100

    implement dynamic csv formatting

--- src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvFormatter.kt
+++ src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvFormatter.kt
@@ -2,9 +2,28 @@ package com.github.christophpickl.codingdojo.csvviewer
 
 object CsvFormatter {
 
-    fun format(csv: CsvTable): String =
-        csv.headers.joinToString("|") + "\n" +
-            "--+--\n" +
-            csv.rowData.joinToString("\n") { it.joinToString("|") }
+    fun format(csv: CsvTable): String {
+        return csv.headers.mapIndexed { index, header ->
+            header.ensureLength(csv.columnMaxLengths[index])
+        }.joinToString("|") + "\n" +
+            csv.headers.mapIndexed { index, _ ->
+                "-".times(csv.columnMaxLengths[index])
+            }.joinToString("+") + "\n" +
+            csv.rowData.mapIndexed { _, row ->
+                row.mapIndexed { index, rowEntry ->
+                    rowEntry.ensureLength(csv.columnMaxLengths[index])
+                }.joinToString("|")
+            }.joinToString("\n")
+    }
+
+    private fun String.ensureLength(desiredLength: Int): String {
+        if (length >= desiredLength) {
+            return this
+        }
+        return this + " ".times(desiredLength - length)
+    }
+
+    private fun String.times(desiredLength: Int) =
+        (1..desiredLength).fold("") { acc, _ -> "$acc$this" }
 
 }
