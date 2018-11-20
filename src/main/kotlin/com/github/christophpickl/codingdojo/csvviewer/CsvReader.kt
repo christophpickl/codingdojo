0150b955d546c158e1a18b4e954f460f2319798d
christophpickl <christoph.pickl@gmail.com>
2018-11-20 20:45:49 +0100

    finish naive read csv logic

--- src/main/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvReader.kt
+++ src/main/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvReader.kt
@@ -1,7 +1,21 @@
 package com.github.christophpickl.codingdojo.csvviewer
 
 object CsvReader {
+
     fun read(content: String): CsvTable {
-        return CsvTable(listOf(CsvColumn("x", emptyList())))
+        val lines = content.lines()
+        val headers = lines[0].split(";")
+        val entriesByColumn = 1.rangeTo(headers.size).map { ArrayList<String>() }
+        lines.subList(1, lines.size).forEach { line ->
+            val lineData = line.split(";")
+            lineData.forEachIndexed { index, entry ->
+                entriesByColumn[index] += entry
+            }
+        }
+        val columns = entriesByColumn.mapIndexed { index, entries ->
+            CsvColumn(headers[index], entries)
+        }
+        return CsvTable(columns)
     }
+
 }
