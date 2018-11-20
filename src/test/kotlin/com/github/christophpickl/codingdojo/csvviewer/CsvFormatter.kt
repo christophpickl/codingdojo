ab425913cda6514f23eb8ce512718be1c25cfd7c
christophpickl <christoph.pickl@gmail.com>
2018-11-20 21:31:12 +0100

    refactoring

--- src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvFormatter.kt
+++ src/test/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvFormatter.kt
@@ -7,26 +7,25 @@ object CsvFormatter {
     private val underline = "-"
     private val newLine = "\n"
 
-    fun format(csv: CsvTable): String {
-        return csv.headers.mapIndexed { index, header ->
-            header.ensureLength(csv.columnMaxLengths[index])
+    fun format(csv: CsvTable): String =
+        csv.formatTable()
+
+    private fun CsvTable.formatTable() =
+        headers.mapIndexed { index, header ->
+            header.ensureLength(columnMaxLengths[index])
         }.joinToString(entrySeparator) + newLine +
-            csv.headers.mapIndexed { index, _ ->
-                underline.times(csv.columnMaxLengths[index])
+            headers.mapIndexed { index, _ ->
+                underline.times(columnMaxLengths[index])
             }.joinToString(headerSeparator) + newLine +
-            csv.rowData.mapIndexed { _, row ->
+            rowData.mapIndexed { _, row ->
                 row.mapIndexed { index, rowEntry ->
-                    rowEntry.ensureLength(csv.columnMaxLengths[index])
+                    rowEntry.ensureLength(columnMaxLengths[index])
                 }.joinToString(entrySeparator)
             }.joinToString(newLine)
-    }
 
-    private fun String.ensureLength(desiredLength: Int): String {
-        if (length >= desiredLength) {
-            return this
-        }
-        return this + " ".times(desiredLength - length)
-    }
+    private fun String.ensureLength(desiredLength: Int) =
+        if (length >= desiredLength) this
+        else this + " ".times(desiredLength - length)
 
     private fun String.times(desiredLength: Int) =
         (1..desiredLength).fold("") { acc, _ -> "$acc$this" }
