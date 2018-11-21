a22924d6090de4818d77ae86e38ab893a1473848
christophpickl <christoph.pickl@gmail.com>
2018-11-21 18:10:43 +0100

    outsource starter class (configurable)

--- src/main/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvViewerApp.kt
+++ src/main/kotlin/com/github/christophpickl/codingdojo/csvviewer/CsvViewerApp.kt
@@ -9,26 +9,33 @@ object CsvViewerApp {
 
     @JvmStatic
     fun main(cliArgs: Array<String>) {
+        val starter = CsvViewerStarter(io = io)
         when (val args = ArgsParser.parse(cliArgs)) {
             is Args.RightArgs -> {
-                start(args)
+                try {
+                    starter.start(args)
+                } catch (e: Exception) {
+                    io.println("[EXCEPTION] ${e.message ?: "N/A"}")
+                }
             }
             is Args.WrongArgs -> {
                 io.println(args.message)
             }
         }
     }
+}
 
-    private fun start(args: Args.RightArgs) {
-        try {
-            val table = Reader.read(readLinesOf(args.csvFile))
-            val viewer = CsvViewer(table = table, pageSize = args.pageSize)
-            viewer.startCommandLoop()
-        } catch (e: Exception) {
-            io.println("[EXCEPTION] ${e.message ?: "N/A"}")
-        }
+class CsvViewerStarter(
+    private val io: InputOutput = SystemInputOutput
+) {
+
+    fun start(args: Args.RightArgs) {
+        val table = Reader.read(readLinesOf(args.csvFile))
+        val viewer = CsvViewer(table = table, pageSize = args.pageSize)
+        viewer.startCommandLoop()
     }
 
+    // TODO outsource (via IO)
     private fun readLinesOf(classpath: String): List<String> {
         val resource = javaClass.getResource("/csvviewer/$classpath")
             ?: throw IllegalArgumentException("File does not exist at classpath: $classpath")
